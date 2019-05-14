c21028d9aa07892e8f99eafcbb2cbc28d96dd995
Raymond <180527e@mymail.nyp.edu.sg>
2019-05-15 01:18:45 +0800

    mostly just modified css and added random stuff to test cher version of  how he use his database so just ignore those files

--- models/User.js
+++ models/User.js
@@ -1,17 +1,14 @@
-// const Sequelize = require('sequelize');
-// const db = require('../config/DBConfig');
+const Sequelize = require('sequelize');
+const db = require('../config/DBConfig');
 /* Creates a user(s) table in MySQL Database.
 Note that Sequelize automatically pleuralizes the entity name as the table name
 */
-// const User = db.define('user', {
-    // name: {
-        // type: Sequelize.STRING
-    // },
-    // email: {
-        // type: Sequelize.STRING
-    // },
-    // password: {
-        // type: Sequelize.STRING
-    // },
-// });
-// module.exports = User;
+const User = db.define('user', {
+    email: {
+        type: Sequelize.STRING
+    },
+    password: {
+        type: Sequelize.STRING
+    },
+});
+module.exports = User;
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,61 @@
-//const express = require('express');
-//const router = express.Router();
-// const User = require('../models/User');
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+router.get('/', (req, res) => {
+    const title = 'Smart Food';
+    res.render('home', {title: title}) // renders views/home.handlebars
+});
+
+router.post('/register', (req, res) => {
+
+    let { email, password, confirmpassword } = req.body;
+
+    // Checks if both passwords entered are the same
+//    if (password !== confirmpassword) {
+//        errors.push({ text: 'Passwords do not match' });
+//    }
+
+    // Checks that password length is more than 4
+//    if (password.length < 4) {
+//        errors.push({ text: 'Password must be at least 4 characters' });
+//    }
+//    if (errors.length > 0) {
+//        res.render('user/register', {
+//            errors,
+//            name,
+//            email,
+//            password,
+//            password2
+//        });
+//    } else {
+//        // If all is well, checks if user is already registered
+//        User.findOne({ where: { email: req.body.email } })
+//            .then(user => {
+//                if (user) {
+                    // If user is found, that means email has already been
+                    // registered
+//                    res.render('user/register', {
+//                        error: user.email + ' already registered',
+//                        name,
+//                        email,
+//                        password,
+//                        password2
+//                    });
+//                } else {
+                    // Encrypt the password
+//                    var salt = bcrypt.genSaltSync(10);
+//                    var hashedPassword = bcrypt.hashSync(password, salt);
+//                    password = hashedPassword;
+
+                    // Create new user record
+                    User.create({ email, password })
+                        .then(user => {
+                            alertMessage(res, 'success', email.name + ' added. Please login', 'fas fa-sign-in-alt', true);
+                            res.redirect('/loginuser');
+                        })
+                        .catch(err => console.log(err));
+//                }
+//            });
+ //   }
+});
