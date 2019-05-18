cd6438ee2bdc25bb4feb3bf08901bc8ce963f45f
184451j-nyp <184451j@mymail.nyp.edu.sg>
2019-05-19 00:32:04 +0800

    commit

--- legin/telegramNoti.js
+++ legin/telegramNoti.js
@@ -27,7 +27,7 @@ bot.on('message', function (msg) {/* <function (msg)> or <(msg) => > */
     var echo = msg.text;
     // send message
     bot.sendMessage(id, echo);
-    bot.sendMessage('239513475', echo);
+    //bot.sendMessage('239513475', echo);
 });
 
 
