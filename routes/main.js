09b0811cedaf8af871b029b904a86cc62d344fbd
Raymond <180527e@mymail.nyp.edu.sg>
2019-05-20 12:24:20 +0800

    test

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router();
+const alertMessage = require('../helpers/messenger')
+
 
 router.get('/', (req, res) => {
     const title = 'Smart Food';
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+var bcrypt = require('bcryptjs');
+var mysql = require("mysql");
+
 
 router.get('/', (req, res) => {
     const title = 'Smart Food';
@@ -9,53 +12,67 @@ router.get('/', (req, res) => {
 
 router.post('/register', (req, res) => {
 
-    let { email, password, confirmpassword } = req.body;
+    let { name, adminnumber, password, confirmpassword, phonenumber, media } = req.body;
 
     // Checks if both passwords entered are the same
-//    if (password !== confirmpassword) {
-//        errors.push({ text: 'Passwords do not match' });
-//    }
-
+    if (password !== confirmpassword) {
+        errors.push({ text: 'Passwords do not match' });
+    }
+    else {
+        res.render('user/register', {
+            errors,
+            name,
+            adminnumber,
+            password,
+            confirmpassword,
+            phonenumber,
+            media
+        });
+    }
     // Checks that password length is more than 4
-//    if (password.length < 4) {
-//        errors.push({ text: 'Password must be at least 4 characters' });
-//    }
-//    if (errors.length > 0) {
-//        res.render('user/register', {
-//            errors,
-//            name,
-//            email,
-//            password,
-//            password2
-//        });
-//    } else {
-//        // If all is well, checks if user is already registered
-//        User.findOne({ where: { email: req.body.email } })
-//            .then(user => {
-//                if (user) {
-                    // If user is found, that means email has already been
-                    // registered
-//                    res.render('user/register', {
-//                        error: user.email + ' already registered',
-//                        name,
-//                        email,
-//                        password,
-//                        password2
-//                    });
-//                } else {
+    if (password.length < 4) {
+        errors.push({ text: 'Password must be at least 4 characters' });
+    }
+    if (errors.length > 0) {
+        res.render('user/register', {
+            errors,
+            name,
+            adminnumber,
+            password,
+            confirmpassword,
+            phonenumber,
+            media
+        });
+    } else {
+        // If all is well, checks if user is already registered
+        User.findOne({ where: { adminnumber: req.body.adminnumber } })
+            .then(user => {
+                if (user) {
+                    // If user is found, that means email has already been registered
+                    res.render('user/register', {
+                        error: user.adminnumber + ' already registered',
+                        name,
+                        adminnumber,
+                        password,
+                        confirmpassword,
+                        phonenumber,
+                        media
+                    });
+                } else {
                     // Encrypt the password
-//                    var salt = bcrypt.genSaltSync(10);
-//                    var hashedPassword = bcrypt.hashSync(password, salt);
-//                    password = hashedPassword;
+                    var salt = bcrypt.genSaltSync(10);
+                    var hashedPassword = bcrypt.hashSync(password, salt);
+                    password = hashedPassword;
 
                     // Create new user record
-                    User.create({ email, password })
+                    User.create({ name, adminnumber, password, phonenumber, media})
                         .then(user => {
-                            alertMessage(res, 'success', email.name + ' added. Please login', 'fas fa-sign-in-alt', true);
+                            alertMessage(res, 'success', adminnumber.name + ' added. Please login', 'fas fa-sign-in-alt', true);
                             res.redirect('/loginuser');
-                        })
+                       })
                         .catch(err => console.log(err));
-//                }
-//            });
- //   }
+                }
+            });
+    }
 });
+module.exports = router;
