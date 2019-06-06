1d8474dc115c0399b6e3e839f6e1dbfadd512e65
Raymond <180527e@mymail.nyp.edu.sg>
2019-06-06 11:14:29 +0800

    smth

--- routes/user.js
+++ routes/user.js
@@ -1,38 +1,56 @@
 const express = require('express');
 const router = express.Router();
-var mysql = require("mysql2");
 
-var con = mysql.createConnection({
-    host: "bloopy.dyndns-home.com",
-    //host: "diskstation",
-    port: 3307,
-    user: "guest",
-    password: "password",
-    database: "ooadp"
-
-});
 router.get('/', (req, res) => {
     const title = 'Smart Food';
     res.render('home', {title: title}) // renders views/home.handlebars
 });
 
 router.post('/register', (req, res) =>{
-    var userId = function () {
-        return '_' + Math.random().toString(36).substr(2, 9);
-    };
-    let { name, adminnumber, password, telegramId, phonenumber } = req.body;
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        var sql = "INSERT INTO User(userId, name, adminnumber, password, telegramid, phonenumber) VALUES ?";
-        var values = [userId, name, adminnumber, password, telegramId, phonenumber]
-        con.query(sql, [values], function (err) {
-            if (err) throw err;
-            console.log("1 record inserted");
-            con.end();
-    res.redirect('/loginuser');
-        });
-    });
+    // var userId = function () {
+    //    return '_' + Math.random().toString(36).substr(2, 9);
+    //};
+    let { name, adminNo, password, confirmpassword, telegramId, phonenumber } = req.body;
+    if (isNaN(name)){
+        if (password == confirmpassword) {
+            if (adminNo.length == 7 && adminNo.endsWith("a")){
+                var adminchar = adminNo.slice(0, 6);
+                if (isNaN(adminchar)){
+                }
+                else{
+                    if (isNaN(telegramId)){
+                        
+                    }
+                    else{
+                        if (telegramId.length == 9){
+                            if (phonenumber.length == 8){
+                                if (isNaN(phonenumber)){
+
+                                }
+                                else{
+                                    res.redirect('/loginuser');
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+    
+    //con.connect(function(err) {
+    //    if (err) throw err;
+    //    console.log("Connected!");
+    //    var sql = "INSERT INTO User(userId, name, adminnumber, password, telegramid, phonenumber) VALUES ?";
+    //    var values = [userId, name, adminnumber, password, telegramId, phonenumber]
+    //    con.query(sql, [values], function (err) {
+    //        if (err) throw err;
+    //        console.log("1 record inserted");
+    //        con.end();
+    
+    //    });
+    //});
 });
 
 module.exports = router;
