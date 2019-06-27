9102a0fe0f77dafcde583b836c6cb04adcbd1b63
Legin <180448w@mymail.nyp.edu.sg>
2019-06-27 23:38:58 +0800

    telegram update db

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ const userRoute = require('./routes/user');
 const app = express();
 
 app.engine('handlebars', exphbs({
-    // Specify default template views/user/home.handlebar
-})); 
+	// Specify default template views/user/home.handlebar
+}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({
-    extended: false
+	extended: false
 }));
 
 /*
@@ -58,18 +58,78 @@ db.setUpDB(false); // To set up database with new tables set (true)
 const port = 5000;
 
 app.listen(port, () =>{
-    console.log(`Server started on port ${port}`);
+	console.log(`Server started on port ${port}`);
 });
 
-
+var mysql = require('mysql2');
 const bot = require("./config/telegramConfig");
+
+var con = mysql.createConnection({
+	host: "bloopy.dyndns-home.com",
+	port: "3307",
+	user: "guest",
+	password: "password",
+	database: "ooadp"
+  });
+
 // catch message
 bot.on('message', function (msg) {/* <function (msg)> or <(msg) => > */
-    console.log(msg);
-    // get sender id
+	console.log(msg);
+	// get sender id
 	var sender = msg.chat.id;
-	User.create({user_id:"dwsa", admin_no:"w122", full_name:"qwer", password:"ggad", phone_no:"12345678", telegram_id:sender, admin_status: 0})
-    // get text
+	//User.create({ user_id: "1", admin_no: "180448w", full_name: "Nigel Cheong", password: "pls_encrypt_this", phone_no: "12345678", telegram_id: null, admin_status: null })
+	// get text
 	var content = msg.text;
 	
 });
+
+/*
+con.connect(function(err) {
+	if (err) throw err;
+	var him = "'180448w'";
+	
+	var sql = "UPDATE ooadp.users SET telegram_id = '2313' WHERE admin_no = " + him ;
+	console.log (sql)
+	con.query(sql, function (err, result) {
+		if (err) throw err;
+		console.log(result.affectedRows + " record(s) updated");
+	});
+});
+*/
+
+// Matches "/echo [whatever]"
+bot.onText(/\/start (.+)/, (msg, match) => {
+	// 'msg' is the received Message from Telegram
+	// 'match' is the result of executing the regexp above on the text content
+	// of the message
+   
+	const chatId = msg.chat.id;
+	const resp = "Hi there, thank you for signing up! \xF0\x9F\x98\x8A"; // the captured "whatever"
+   
+	// send back the matched "whatever" to the chat
+	bot.sendMessage(chatId, resp);
+  });
+
+  bot.onText(/\/verify (.+)/, (msg, match) => {
+	// 'msg' is the received Message from Telegram
+	// 'match' is the result of executing the regexp above on the text content
+	// of the message
+   
+	var chatId = msg.chat.id;
+	var resp = match[1]; // the captured "whatever"
+	console.log('qwertyqweqqweqweqweqe')
+	con.connect(function(err) {
+		if (err) throw err;
+		var him = "'180448w";
+		console.log("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqq")
+		var sql = "UPDATE ooadp.users SET telegram_id = '" + chatId + "'" + "WHERE admin_no = '180448w'" ;
+		console.log (sql)
+		con.query(sql, function (err, result) {
+			if (err) throw err;
+			console.log(result.affectedRows + " record(s) updated");
+		});
+	});
+	console.log('wdadasdawdtttttttttttttttttttttttttttttttttt')
+	// send back the matched "whatever" to the chat
+	bot.sendMessage(chatId, resp);
+  });
