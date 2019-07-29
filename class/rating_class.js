4f5bea6820f7f394ec387e1c4547a521e65b6971
Legin <180448w@mymail.nyp.edu.sg>
2019-07-29 21:42:07 +0800

    able to save rating into db

--- class/rating_class.js
+++ class/rating_class.js
@@ -1,24 +1,31 @@
 const RatingModel = require('../models/Rating');
+const op = require('sequelize').Op;
 var ex = module.exports = {};
 
-ex.getUserByAdmin = function(adminNo){
+ex.getUserByAdmin = function (adminNo) {
     return UserModel.findByPk(adminNo)
-    .catch(err => {
-        console.log(err)
-    });
+        .catch(err => {
+            console.log(err)
+        });
 };
 
-ex.averageRating = function(entityID){
+ex.averageRating = function (entityID) {
     return RatingModel.sum('rating_given', {
-        where: {item_id: entityID}
+        where: { item_id: entityID }
     }).then(sum => {
         RatingModel.count({
-            where: {item_id: entityID},
+            where: { item_id: entityID },
             raw: true
         }).then(count => {
-            console.log((sum/count).toFixed(1))
+            console.log((sum / count).toFixed(1))
         }).catch(err => {
             console.log(err)
         })
     })
 };
+
+ex.count = function () {
+    return RatingModel.count({
+
+    })
+}
