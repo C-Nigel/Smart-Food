5d30241a55a5bb610ca52e3120120d175b976d7c
Legin <180448w@mymail.nyp.edu.sg>
2019-08-01 16:18:48 +0800

    updated ratings page

--- routes/ratings.js
+++ routes/ratings.js
@@ -5,29 +5,19 @@ const item_class = require('../class/item_class');
 const ensureAuthenticated = require('../helpers/auth');
 const orders = require('../class/order_class');
 const rating_class = require('../class/rating_class');
-const op = require('sequelize').Op;
-
+const Op = require('sequelize').Op;
+const sequelize = require('sequelize')
+const dbconfig = require('../config/DBConfig')
 
 router.get('/:user_admin', (req, res) => {
-
-    rating.findAll({
-        where: {
-            user_admin: req.params.user_admin,
-            rating_given: null
-        },
-        raw: true
-    })
-        .then((ratings) => {
-            // orders.getOrderByUser()
-            // pass object to ratings.handlebar
+    dbconfig.query('SELECT ratings.id AS entity_index,users.full_name, ratings.user_admin, ratings.item_id, items.name AS item_name, items.cat, items.price, items.picture_url, items.outlet_id, outlets.name AS outlet_name, ratings.rating_given, ratings.createdAt FROM ooadp.ratings, ooadp.items, ooadp.outlets, ooadp.users WHERE ooadp.ratings.item_id = ooadp.items.id  AND ooadp.items.outlet_id = ooadp.outlets.id AND ooadp.ratings.user_admin = ooadp.users.admin_no AND ratings.rating_given IS NULL AND ratings.user_admin = "' + req.params.user_admin + '"' + 'ORDER BY ratings.createdAt')
+    .then(([ratings, metadata]) => {
             console.log(ratings);
             res.render('../views/ratings', {
                 ratings: ratings,
                 user_admin: req.params.user_admin
             });
-        })
-        .catch(err => console.log(err));
-
+        }).catch(err => console.log(err));
 });
 
 router.post('/saveRating/:admin_no', (req, res) => {
@@ -40,7 +30,7 @@ router.post('/saveRating/:admin_no', (req, res) => {
             var admin_no = req.params.admin_no;
             var numToString = i.toString();
             var rating_given = req.body['rateID_' + numToString];
-            if (rating_given != undefined){
+            if (rating_given != undefined) {
                 rating.update({
                     rating_given
                 }, {
