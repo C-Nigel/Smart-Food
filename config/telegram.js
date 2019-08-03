e64e519104a8c9bb00e2a6d17cdbcbc82bde553f
malique <180723F@mymail.nyp.edu.sg>
2019-08-03 22:53:52 +0800

    s

--- config/telegram.js
+++ config/telegram.js
@@ -4,6 +4,6 @@ var TelegramBot = require('node-telegram-bot-api');
 // token
 var token = '807750194:AAEd6To8ZonJwSUiCf0rSUh9Vqcz-_pTswg';
 // polling option
-var opt = {polling: true};
+var opt = {polling: false};
 // create bot
 module.exports = new TelegramBot(token, opt);
